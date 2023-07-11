03d392a620b3cd553d40af5f6f702dc1afb1a260
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-11 17:19:12 +0530

    authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+
+const auth = async (req, res, next) => {
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      const token = req.headers.authorization.split(" ")[1]
+
+      const decode = jwt.verify(token, process.env.JWT__SECRET)
+      req.user = await User.findOne({ email })
+      next()
+    }
+  } catch (error) {
+    return res.status(400).send("Unauthorized")
+  }
+}
+
+module.exports = auth
