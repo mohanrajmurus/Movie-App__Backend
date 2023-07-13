ea99bd7c9b557e0e7391a53294e41b47af156d2f
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-13 18:18:55 +0530

    asynchandler

--- backend/controllers/MovieController.js
+++ backend/controllers/MovieController.js
@@ -1,58 +1,37 @@
 const movieModel = require("../models/MovieModel")
+const asyncHandler = require("express-async-handler")
+const createMovieDetails = asyncHandler(async (req, res) => {
+  const { title, genre, description, thumbnail, imageURL, videoURL } = req.body
+  if (!title || !genre || !description || !thumbnail || !imageURL || !videoURL)
+    return res.status(400).send("All inputs are required")
+  if (await movieModel.findOne({ title }))
+    return res.status(409).send("Movie title Already Exist.")
+  const movieDetails = await movieModel.create({
+    title,
+    genre,
+    description,
+    thumbnail,
+    imageURL,
+    videoURL,
+    user: req.user._id,
+  })
+  res.status(201).json(movieDetails)
+})
 
-const createMovieDetails = async (req, res) => {
-  try {
-    const { title, genre, description, thumbnail, imageURL, videoURL } =
-      req.body
-    if (
-      !title ||
-      !genre ||
-      !description ||
-      !thumbnail ||
-      !imageURL ||
-      !videoURL
-    )
-      return res.status(400).send("All inputs are required")
-    if (await movieModel.findOne({ title }))
-      return res.status(409).send("Movie title Already Exist.")
-    const movieDetails = await movieModel.create({
-      title,
-      genre,
-      description,
-      thumbnail,
-      imageURL,
-      videoURL,
-      user:req.user._id
-    })
-    res.status(201).json(movieDetails)
-  } catch (error) {
-    console.error(error)
-  }
-}
-const getAllMovieDetails = async (req, res) => {
-  try {
-    const movieList = await movieModel.find({})
-    return res.status(200).json(movieList)
-  } catch (error) {
-    console.error(error)
-  }
-}
-const getMovieById = async (req, res) => {
-  try {
-    const { id } = req.params
-    const movie = await movieModel.findById(id)
-    return res.status(200).json(movie)
-  } catch (error) {
-    console.error(error)
-  }
-}
-const deleteMovieById = async (req, res) => {
-  try {
-    const { id } = req.params
-    const del = await movieModel.findByIdAndDelete(id)
-    res.status(200).json(del)
-  } catch (error) {}
-}
+const getAllMovieDetails = asyncHandler(async (req, res) => {
+  const movieList = await movieModel.find({})
+  return res.status(200).json(movieList)
+})
+const getMovieById = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  const movie = await movieModel.findById(id)
+  return res.status(200).json(movie)
+})
+const deleteMovieById = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  const del = await movieModel.findByIdAndDelete(id)
+  res.status(200).json(del)
+})
 module.exports = {
   createMovieDetails,
   getAllMovieDetails,
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,35 +1,32 @@
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
-const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body
-    if (!name || !email || !password) {
-      return res.status(400).send("All inputs are required")
-    }
-    const existUser = await User.findOne({ email })
-    if (existUser) {
-      return res.status(401).send("User already exist.")
-    }
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(password, salt)
-    const user = await User.create({
-      name,
-      email,
-      password: hashPassword,
-    })
-    if (user) {
-      return res.status(201).json({
-        id: user._id,
-        name: user.name,
-        email: user.email,
-      })
-    } else res.status(400).send("Invalid Parameters")
-  } catch (error) {
-    return res.status(400).send(error)
+const asyncHandler = require("express-async-handler")
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    return res.status(400).send("All inputs are required")
   }
-}
-const loginUser = async (req, res) => {
+  const existUser = await User.findOne({ email })
+  if (existUser) {
+    return res.status(401).send("User already exist.")
+  }
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(password, salt)
+  const user = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  })
+  if (user) {
+    return res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else res.status(400).send("Invalid Parameters")
+})
+const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
   if (!email || !password) {
     res.status(400).send("All inputs are required")
@@ -42,7 +39,7 @@ const loginUser = async (req, res) => {
         name: user.name,
         email: user.email,
         token: generateJWTToken(user.email),
-        isAdmin:user.isAdmin
+        isAdmin: user.isAdmin,
       })
     } else {
       return res.status(400).send("Invalid Password")
@@ -50,33 +47,27 @@ const loginUser = async (req, res) => {
   } else {
     return res.status(400).send("User not registred with us")
   }
-}
-const findUser = async(req,res) => {
-  const {id} = req.params
-  try {
-    console.log(id);
-    const user = await User.findOne({email:id})
-    if(user) {
-      return res.status(200).json({
-        id:user._id
-      })
-    }
-    else return res.status(400).send('Email not registred with us. Please Create Account')
-  } catch (error) {
-    return res.status(400).send(error)
-  }
-}
-const resetPassword = async(req,res) => {
-  try {
-    const {id} = req.params
-    const {password} = req.body
-    const hashPassword = await bcrypt.hash(password,10)
-    const user = await User.findByIdAndUpdate(id,{password:hashPassword})
-    return res.status(200).send('password changed sucessfully')
-  } catch (error) {
-    return res.status(400).send(error)
-  }
-}
+})
+
+const findUser = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  const user = await User.findOne({ email: id })
+  if (user) {
+    return res.status(200).json({
+      id: user._id,
+    })
+  } else
+    return res
+      .status(400)
+      .send("Email not registred with us. Please Create Account")
+})
+const resetPassword = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  const { password } = req.body
+  const hashPassword = await bcrypt.hash(password, 10)
+  const user = await User.findByIdAndUpdate(id, { password: hashPassword })
+  return res.status(200).send("password changed sucessfully")
+})
 
 const generateJWTToken = (email) => {
   return jwt.sign({ email }, process.env.JWT__SECRET, {
@@ -84,4 +75,4 @@ const generateJWTToken = (email) => {
   })
 }
 
-module.exports = { registerUser, loginUser, findUser,resetPassword }
+module.exports = { registerUser, loginUser, findUser, resetPassword }
